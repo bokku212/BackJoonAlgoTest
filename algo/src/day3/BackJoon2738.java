a35d63effde9fef5e612da18ddc700adb222b318
bokku <ddgag38@gmail.com>
2026-01-12 17:10:21 +0900

    2738 복습

--- algo/src/day3/BackJoon2738.java
+++ algo/src/day3/BackJoon2738.java
@@ -10,6 +10,7 @@ public class BackJoon2738 {
 
     //제목 : 행렬 덧셈
     //https://www.acmicpc.net/problem/2738
+    //260111 제출 풀이..2차원 배열..어렵..
     public static void main(String[] args) throws Exception {
         //Scanner 사용
 //        Scanner sc = new Scanner(System.in);
@@ -53,11 +54,11 @@ public class BackJoon2738 {
 
         int[][] arrA = new int[N][M];
 
-        //행렬A입력
+        //행렬A:입력+저장
         for(int i=0;i<N;i++){
-           st = new StringTokenizer(br.readLine()); //행으로 읽어온걸 여백기준 토큰분리
+           st = new StringTokenizer(br.readLine()); //행으로 읽어온걸 여백기준 토큰분리,이미 위에서 존재하는 st변수에 새로운 StringTokenizer를 다시 넣는거임
             for(int j=0;j<M;j++){
-                arrA[i][j]=Integer.parseInt(st.nextToken());
+                arrA[i][j]=Integer.parseInt(st.nextToken()); //나중에 다시 쓰기위한 배열저장, 여기st는 for문안에서 선언된 st임
             }
         }
 
@@ -67,7 +68,7 @@ public class BackJoon2738 {
         for(int i=0;i<N;i++){
             st = new StringTokenizer(br.readLine());
             for(int j=0;j<M;j++){
-                int b = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken()); // 나중에 안씀, 당장쓰고말거라서 배열아닌 임시변수 하나로 충분,이 for문 순간순간에 해당하는 행렬B의 값하나씩 b에 들어감
                 sb.append(arrA[i][j]+b); // .append : 출력아닌 문자열을 StringBuilder에 저장하는 동작
                 if(j<M-1) sb.append(" ");
             }
